5b5826216f45b6312fac4043a88ccada3e4e085e
yoonani <yoonani72@gmail.com>
2015-07-16 16:44:56 +0900

    conf test

---
+++ conf.php
@@ -0,0 +1,11 @@
+<?php
+define("SCHEME", "http://");
+define("HOST", SCHEME . "medis.hallym.ac.kr/");
+define("WEB_ROOT", HOST . "dv/");
+define("DOC_ROOT", "/home/yoonani/dv/");
+define("DATA_ROOT", "/home/yoonani/dv.data/");
+
+define("LIB", WEB_ROOT . "lib/");
+define("JS", LIB . "js/");
+
+?>
