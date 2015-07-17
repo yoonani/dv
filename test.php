ccaaa3baa54328ee500471c80debfab2d590b31d
yoonani <yoonani72@gmail.com>
2015-07-17 14:00:50 +0900

    second day....

--- test.php
+++ test.php
@@ -6,7 +6,7 @@ require_once("conf.php");
 	<head>
 		<script type="text/javascript" src="<?php echo JQ_DIR?>jquery-1.11.3.min.js"></script>
 		<script type="text/javascript" src="<?php echo JQ_DIR?>jquery-migrate-1.2.1.min.js"></script>
-		<script type="text/javascript" src="<?php echo D3?>jquery-migrate-1.2.1.min.js"></script>
+		<script type="text/javascript" src="<?php echo D3?>"></script>
 	</head>
 	<body>
 		
