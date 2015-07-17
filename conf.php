cfc962ece0e66a66bf7d52163f8ba88cf184ee3e
yoonani <yoonani72@gmail.com>
2015-07-17 13:38:49 +0900

    conf test

---
+++ conf.php
@@ -0,0 +1,13 @@
+<?php
+define("SCHEME", "http://");
+define("HOST", SCHEME . "medis.hallym.ac.kr/");
+define("WEB_ROOT", HOST . "dv/");
+define("DOC_ROOT", "/home/yoonani/dv/");
+define("DATA_ROOT", "/home/yoonani/dv.data/");
+
+define("LIB", WEB_ROOT . "lib/");
+define("JS", LIB . "js/");
+define("D3", JS . "d3/d3.min.js");
+define("JQ_DIR", JS . "jQuery/");
+define("BS_DIR", JS . "bootstrap/");
+?>
---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+require_once("conf.php");
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<script type="text/javascript" src="<?php echo JQ_DIR?>jquery-1.11.3.min.js"></script>
+		<script type="text/javascript" src="<?php echo JQ_DIR?>jquery-migrate-1.2.1.min.js"></script>
+	</head>
+	<body>
+		
+	</body>
+</html>
