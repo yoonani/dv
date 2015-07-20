67d070f2a866c0771815dcbc993a7048011b54b0
yoonani <yoonani72@gmail.com>
2015-07-20 22:47:02 +0900

    Add Barchat.html

--- test.php
+++ test.php
@@ -7,6 +7,27 @@ require_once("conf.php");
 		<script type="text/javascript" src="<?php echo JQ_DIR?>jquery-1.11.3.min.js"></script>
 		<script type="text/javascript" src="<?php echo JQ_DIR?>jquery-migrate-1.2.1.min.js"></script>
 		<script type="text/javascript" src="<?php echo D3?>"></script>
+		<style>
+			body { font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;}
+			
+			.box { font: 10px sans-serif;}
+			
+			.box line,
+			.box rect,
+			.box circle {
+			  fill: #fff;
+			  stroke: #000;
+			  stroke-width: 1.5px;
+			}
+			
+			.box .center {  stroke-dasharray: 3,3;}
+			
+			.box .outlier {
+			  fill: none;
+			  stroke: #ccc;
+			}
+
+		</style>
 	</head>
 	<body>
 		
